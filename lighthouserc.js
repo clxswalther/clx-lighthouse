ec5567497fb1aa4b84c98782208c3e0e0f789599
swalther <swalther@complex.it.de>
2020-06-25 12:37:04 +0200

    remove build command from lighthouse.yml

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
     ci: {
         collect: {
-            url: ['https://www.kunumann.de/'],
+            url: ['https://www.kunzmann.de/'],
         },
         upload: {
             target: 'temporary-public-storage',
